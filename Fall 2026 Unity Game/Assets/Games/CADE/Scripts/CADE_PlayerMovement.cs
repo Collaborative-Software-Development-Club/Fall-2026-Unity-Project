f9d2b73bf2f6afb7a7b3ce8952a6c34a3feacf0f
ethanVincent24 <ethanvincent244@gmail.com>
2026-09-11 18:47:56 -0400

    Added jumping (unfinished)
    Can jump but there is no ground check yet

--- Fall 2026 Unity Game/Assets/Games/CADE/Scripts/CADE_PlayerMovement.cs
+++ Fall 2026 Unity Game/Assets/Games/CADE/Scripts/CADE_PlayerMovement.cs
@@ -8,21 +8,31 @@ public class CADE_PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed = 5f;
+    [SerializeField]
+    private float jumpHeight = 10f;
 
     private float horizontalMovement;
-
-    void Start()
-    {
-        
-    }
+    private float verticalMovement;
 
     void Update()
     {
-        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocityY);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && rb.linearVelocityY == 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpHeight);
+        }
+        else if (context.canceled) 
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocityY * 0.5f);
+        }
     }
 }
