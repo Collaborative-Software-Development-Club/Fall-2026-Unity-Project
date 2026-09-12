690991ec632f225a9baa17034394b35e40ee18a8
ethanVincent24 <ethanvincent244@gmail.com>
2026-09-12 17:16:14 -0400

    Ground check for jumps, modifiable gravity and falling speed
    Jumping is now more polished and functional. Still could use some work in terms of improving the game feel, but it works

--- Fall 2026 Unity Game/Assets/Games/CADE/Scripts/CADE_PlayerMovement.cs
+++ Fall 2026 Unity Game/Assets/Games/CADE/Scripts/CADE_PlayerMovement.cs
@@ -6,17 +6,38 @@ public class CADE_PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb;
 
+    [Header("Movement")]
     [SerializeField]
     private float moveSpeed = 5f;
+
+    [Header("Jumping")]
     [SerializeField]
     private float jumpHeight = 10f;
 
+    [Header("Ground Check")]
+    [SerializeField]
+    private Vector2 boxSize;
+    [SerializeField]
+    private float castDistance;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [Header("Gravity")]
+    [SerializeField]
+    private float baseGravity = 1;
+    [SerializeField]
+    private float maxFallSpeed = 18;
+    [SerializeField]
+    private float fallSpeedMultipler = 2f;
+
+    private bool jumpReleased;
     private float horizontalMovement;
     private float verticalMovement;
 
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocityY);
+        Gravity();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -26,13 +47,42 @@ public class CADE_PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && rb.linearVelocityY == 0)
+        if (isGrounded() && context.performed)
         {
+            jumpReleased = false;
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpHeight);
         }
-        else if (context.canceled) 
+        else if (context.canceled && !jumpReleased)
         {
+            jumpReleased = true;
             rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocityY * 0.5f);
         }
     }
+
+    private void Gravity()
+    {
+        if (rb.linearVelocityY < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultipler;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocityY, -maxFallSpeed));
+        }
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+    }
+
 }
